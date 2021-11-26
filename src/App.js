c34d4be1e234b9343ef6ea4d92c1869e14ffae19
swillrich5 <swillrich@gmail.com>
2021-11-25 19:47:29 -0600

    cleanup after initial setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>I Like Tacos!</h1>
+      <h2>Who else likes tacos?</h2>
+    </div>
+  );
+}
+
+export default App;
